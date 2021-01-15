41ea5ee7e298e256b3f349e24ddb17e146c0774c
NightMare8587 <62491774+Ploya-hacker@users.noreply.github.com>
2021-01-15 08:05:17 +0530

    changes made

---
+++ app/src/main/java/com/example/fastwayadmin/MenuActivities/AllMenuDish.java
@@ -0,0 +1,39 @@
+package com.example.fastwayadmin.MenuActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.fastwayadmin.R;
+import com.example.fastwayadmin.SearchDish;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AllMenuDish extends AppCompatActivity {
+    DatabaseReference allMenu;
+    FloatingActionButton search;
+    ProgressBar loading;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_menu_dish);
+        initialise();
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SearchDish.class));
+            }
+        });
+    }
+
+    private void initialise() {
+        allMenu = FirebaseDatabase.getInstance().getReference().getRoot();
+        search = (FloatingActionButton)findViewById(R.id.floatingActionButton2);
+        loading = findViewById(R.id.loading);
+    }
+}
--- app/src/main/java/com/example/fastwayadmin/NavFrags/MenuFrag.java
+++ app/src/main/java/com/example/fastwayadmin/NavFrags/MenuFrag.java
@@ -1,18 +1,22 @@
 package com.example.fastwayadmin.NavFrags;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.example.fastwayadmin.MenuActivities.AllMenuDish;
 import com.example.fastwayadmin.R;
 
 public class MenuFrag extends Fragment {
+    Button mainCourse,breads,snacks,deserts,sweets,drinks;
     Toolbar menuBar;
     @Nullable
     @Override
@@ -24,5 +28,18 @@ public class MenuFrag extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         menuBar = view.findViewById(R.id.menuFragBar);
+        mainCourse = view.findViewById(R.id.MainCourse);
+        breads = view.findViewById(R.id.Breads);
+        snacks = view.findViewById(R.id.Snacks);
+        deserts = view.findViewById(R.id.Deserts);
+        sweets = view.findViewById(R.id.Sweets);
+        drinks = view.findViewById(R.id.Drinks);
+
+        mainCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AllMenuDish.class));
+            }
+        });
     }
 }
