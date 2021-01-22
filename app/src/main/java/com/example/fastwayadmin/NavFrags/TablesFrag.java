44c98870265852cc4cfc23daedb44c6f67dcb119
NightMare8587 <62491774+Ploya-hacker@users.noreply.github.com>
2021-01-22 07:07:17 +0530

    minor changes

--- app/src/main/java/com/example/fastwayadmin/NavFrags/TablesFrag.java
+++ app/src/main/java/com/example/fastwayadmin/NavFrags/TablesFrag.java
@@ -11,9 +11,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.fastwayadmin.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TablesFrag extends Fragment {
     Toolbar tableBar;
+    FloatingActionButton addTable;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,5 +26,13 @@ public class TablesFrag extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tableBar = view.findViewById(R.id.tableBar);
+        addTable = view.findViewById(R.id.addTables);
+
+        addTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
