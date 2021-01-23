70afa50cd4fdc393b0b0d8ab5785e9ee7976cc10
NightMare8587 <62491774+Ploya-hacker@users.noreply.github.com>
2021-01-23 09:09:32 +0530

    bug fixed

--- app/src/main/java/com/example/fastwayadmin/AddTables.java
+++ app/src/main/java/com/example/fastwayadmin/AddTables.java
@@ -3,12 +3,40 @@ package com.example.fastwayadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddTables extends AppCompatActivity {
 
+    EditText tableNumber;
+    EditText numberOfSeats;
+    Button generateQrCode;
+    FirebaseAuth tableAuth;
+    DatabaseReference tableRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tables);
+        initialise();
+
+        generateQrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+
+    private void initialise() {
+        tableNumber = findViewById(R.id.tableNumber);
+        numberOfSeats = findViewById(R.id.numberOfSeats);
+        generateQrCode = findViewById(R.id.genrateCode);
+        tableAuth = FirebaseAuth.getInstance();
+        tableRef = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(tableAuth.getUid());
     }
 }
