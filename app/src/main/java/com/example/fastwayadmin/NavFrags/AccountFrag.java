31f01aa2f1a8ad3f8250665b351d30b61b202a1e
NightMare8587 <62491774+Ploya-hacker@users.noreply.github.com>
2021-02-04 09:42:35 +0530

    added delete account option

--- app/src/main/java/com/example/fastwayadmin/NavFrags/AccountFrag.java
+++ app/src/main/java/com/example/fastwayadmin/NavFrags/AccountFrag.java
@@ -20,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import com.example.fastwayadmin.Login.MainActivity;
 import com.example.fastwayadmin.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,7 +32,8 @@ import java.util.Objects;
 public class AccountFrag extends Fragment {
     Toolbar accountBar;
     EditText fullName,emailAddress;
-    Button changeInfo,saveChanges,logout;
+    FirebaseUser mUser;
+    Button changeInfo,saveChanges,logout,deleteAccount;
     FirebaseAuth accountAuth;
     DatabaseReference accountRef;
     ProgressBar progressBar;
@@ -47,6 +49,7 @@ public class AccountFrag extends Fragment {
         accountBar = view.findViewById(R.id.accountFragBar);
         progressBar = view.findViewById(R.id.pBar);
         fullName = view.findViewById(R.id.AccountName);
+        deleteAccount = view.findViewById(R.id.deleteAccount);
         changeInfo = view.findViewById(R.id.changeInfo);
         saveChanges = view.findViewById(R.id.saveChanges);
         logout = view.findViewById(R.id.Logout);
@@ -55,7 +58,7 @@ public class AccountFrag extends Fragment {
         emailAddress.setEnabled(false);
         accountAuth = FirebaseAuth.getInstance();
         accountRef = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(Objects.requireNonNull(accountAuth.getUid()));
-
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
         accountRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -97,6 +100,32 @@ public class AccountFrag extends Fragment {
             }
         });
 
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+                builder.setTitle("Delete Account")
+                        .setMessage("Are you sure wanna Delete Account!!!")
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                accountRef = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants");
+                                accountRef.child(accountAuth.getUid()).removeValue();
+                                accountAuth.signOut();
+                                mUser.delete();
+                                startActivity(new Intent(getActivity(), MainActivity.class));
+                                Objects.requireNonNull(getActivity()).finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }).create();
+                builder.show();
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
