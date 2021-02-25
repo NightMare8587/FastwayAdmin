ff3c270694ef1320b1c59f61436b2ba2027bc297
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-02-25 09:29:22 +0530

    h

--- app/src/main/java/com/example/fastwayadmin/MenuActivities/CreateDish.java
+++ app/src/main/java/com/example/fastwayadmin/MenuActivities/CreateDish.java
@@ -10,8 +10,12 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +28,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public class CreateDish extends AppCompatActivity {
@@ -102,7 +108,8 @@ public class CreateDish extends AppCompatActivity {
                 }).setNegativeButton("Take Photo", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //IMAGE CAPTURE CODE
+                startActivityForResult(intent, 0);
             }
         }).setNeutralButton("Choose From Storage", new DialogInterface.OnClickListener() {
             @Override
@@ -136,6 +143,15 @@ public class CreateDish extends AppCompatActivity {
             }else{
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
         }
     }
 
