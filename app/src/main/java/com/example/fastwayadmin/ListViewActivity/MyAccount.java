3d1e36d1ff5f5682191c67b59e3f8dc8617dd48a
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-03-04 13:12:42 +0530

    changes maded

--- app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
+++ app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
@@ -1,56 +1,20 @@
 package com.example.fastwayadmin.ListViewActivity;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
 import com.example.fastwayadmin.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyAccount extends AppCompatActivity {
 
-    EditText infoName,infoEmail;
-    FirebaseFirestore db;
-    DocumentReference reference;
-    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
-        initialise();
-        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if(documentSnapshot.exists()){
-                        infoEmail.setText(documentSnapshot.get("email").toString());
-                        infoName.setText(documentSnapshot.get("name").toString());
-                    }
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
 
-            }
-        });
     }
 
-    private void initialise() {
-        infoEmail = findViewById(R.id.infoEmailAddress);
-        infoName = findViewById(R.id.infoPersonName);
-        db = FirebaseFirestore.getInstance();
-        auth = FirebaseAuth.getInstance();
-         reference = db.collection(auth.getUid()).document("info");
-    }
+
 }
