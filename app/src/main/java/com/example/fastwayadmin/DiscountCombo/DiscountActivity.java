b0e8e0928289044609708a56b88fda6b845ae506
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-03-17 09:06:21 +0530

    new activity added

---
+++ app/src/main/java/com/example/fastwayadmin/DiscountCombo/DiscountActivity.java
@@ -0,0 +1,16 @@
+package com.example.fastwayadmin.DiscountCombo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fastwayadmin.R;
+
+public class DiscountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discount);
+    }
+}
