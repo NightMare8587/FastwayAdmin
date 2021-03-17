a3733d2df2ed1d53bcaf987cd1412d264884980c
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-03-17 13:48:53 +0530

    bug fixed :)

--- app/src/main/java/com/example/fastwayadmin/DiscountCombo/DiscountActivity.java
+++ app/src/main/java/com/example/fastwayadmin/DiscountCombo/DiscountActivity.java
@@ -1,23 +1,55 @@
 package com.example.fastwayadmin.DiscountCombo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.fastwayadmin.R;
 import com.example.flatdialoglibrary.dialog.FlatDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class DiscountActivity extends AppCompatActivity {
-
+    DatabaseReference reference;
+    FirebaseAuth auth;
+    List<String> name = new ArrayList<>();
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount);
-        recyclerView = findViewById(R.id.discountActivityRecyclerView);
+        initialise();
+        name.clear();
+        reference.child("List of Dish").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                        if(dataSnapshot1.child("mrp").getValue().toString().equals("no"))
+                        name.add(dataSnapshot1.child("name").getValue(String.class));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         FlatDialog flatDialog = new FlatDialog(DiscountActivity.this);
         flatDialog.setTitle("Choose One Option")
@@ -42,7 +74,7 @@ public class DiscountActivity extends AppCompatActivity {
                                 .setSecondButtonColor(Color.parseColor("#fee9b2"))
                                 .setSecondButtonTextColor(Color.parseColor("#000000"))
                                 .setSecondButtonText("40% OFF ON ALL")
-                                .setThirdButtonText("30% OFF ON ALL")
+                                .setThirdButtonText("ADD YOUR OWN")
                                 .setThirdButtonColor(Color.parseColor("#fbd1b7"))
                                 .setThirdButtonTextColor(Color.parseColor("#000000"))
                                 .setFirstTextFieldHint("Enter How much discount!!")
@@ -59,6 +91,12 @@ public class DiscountActivity extends AppCompatActivity {
                                     public void onClick(View view) {
                                         flatDialog1.dismiss();
                                     }
+                                })
+                                .withThirdButtonListner(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        flatDialog1.dismiss();
+                                    }
                                 }).show();
                         flatDialog.dismiss();
                     }
@@ -66,8 +104,16 @@ public class DiscountActivity extends AppCompatActivity {
                 .withSecondButtonListner(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        recyclerView.setLayoutManager(new LinearLayoutManager(DiscountActivity.this));
+                        recyclerView.setAdapter(new DiscountRecycler(name));
                         flatDialog.dismiss();
                     }
                 }).show();
+    }
+
+    private void initialise() {
+        recyclerView = findViewById(R.id.discountActivityRecyclerView);
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(Objects.requireNonNull(auth.getUid()));
     }
 }
