39ed260d317ff19c2ef5ce136e8c7ab57f2f6e16
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-03-29 08:44:08 +0530

    oiwhoiqfon

--- app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
+++ app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
@@ -2,7 +2,12 @@ package com.example.fastwayadmin.ListViewActivity;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +16,27 @@ import com.example.fastwayadmin.R;
 
 public class MyAccount extends AppCompatActivity {
     TextView textView;
+    ListView listView;
+    LinearLayout linearLayout;
+    String[] names = {"Change Credentials","My Transactions","Change Mobile Number","Delete Account"};
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
         initialise();
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,names);
+        listView.setAdapter(arrayAdapter);
         SharedPreferences sharedPreferences = getSharedPreferences("loginInfo",MODE_PRIVATE);
         textView.setText("Hi, " + String.valueOf(sharedPreferences.getString("name","")));
+
     }
+
+
 
     private void initialise() {
         textView = findViewById(R.id.account_activity_text);
+        listView = findViewById(R.id.accountActivityListView);
+        linearLayout = findViewById(R.id.linearLayout);
     }
-
-
 }
