0f30738a27ce748e46537a15be2e1509f8072273
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-03-29 08:00:51 +0530

    ubiub

--- app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
+++ app/src/main/java/com/example/fastwayadmin/ListViewActivity/MyAccount.java
@@ -1,19 +1,28 @@
 package com.example.fastwayadmin.ListViewActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fastwayadmin.R;
 
 public class MyAccount extends AppCompatActivity {
-
-
+    TextView textView;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+        initialise();
+        SharedPreferences sharedPreferences = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        textView.setText("Hi, " + String.valueOf(sharedPreferences.getString("name","")));
+    }
 
+    private void initialise() {
+        textView = findViewById(R.id.account_activity_text);
     }
 
 
