35c6aadf925054febad8dea8abf58dc7fa385db2
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-04-19 09:24:20 +0530

    minor changes

--- app/src/main/java/com/consumers/fastwayadmin/ListViewActivity/MyAccount.java
+++ app/src/main/java/com/consumers/fastwayadmin/ListViewActivity/MyAccount.java
@@ -69,11 +69,7 @@ public class MyAccount extends AppCompatActivity implements ModalBottomSheetDial
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
         initialise();
-        modalBottomSheetDialog = new ModalBottomSheetDialog.Builder()
-                .setRoundedModal(true)
-                .setHeader("Choose One Option")
-                .add(R.menu.bottom_sheet)
-                .build();
+
         ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, R.layout.list,names);
         listView.setAdapter(arrayAdapter);
         SharedPreferences sharedPreferences = getSharedPreferences("loginInfo",MODE_PRIVATE);
@@ -92,11 +88,21 @@ public class MyAccount extends AppCompatActivity implements ModalBottomSheetDial
 //                                .setSecondButtonColor(Color.LTGRAY)
 //                                .setSecondButtonText("Change Email")
 //                                .setSecondButtonTextColor()
-
-                        modalBottomSheetDialog.show(getSupportFragmentManager(),"xd");
+                        modalBottomSheetDialog = new ModalBottomSheetDialog.Builder()
+                                .setRoundedModal(true)
+                                .setHeader("Choose One Option")
+                                .add(R.menu.bottom_sheet)
+                                .build();
+                        modalBottomSheetDialog.show(getSupportFragmentManager(),"admin");
 
                         break;
                     case 1:
+                        modalBottomSheetDialog = new ModalBottomSheetDialog.Builder()
+                                .setRoundedModal(true)
+                                .setHeader("Choose One Option")
+                                .add(R.menu.bottom_sheet_restaurant)
+                                .build();
+                        modalBottomSheetDialog.show(getSupportFragmentManager(),"restaurant");
                         break;
                     case 2:
                         auth = FirebaseAuth.getInstance();
@@ -309,85 +315,174 @@ public class MyAccount extends AppCompatActivity implements ModalBottomSheetDial
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onItemSelected(String tag, Item item) {
-        int id = item.getId();
-        switch (id){
-            case R.id.changeNameBottomSheet:
-                FlatDialog flatDialog = new FlatDialog(MyAccount.this);
-                               flatDialog.setTitle("Enter New Name")
-                                       .setBackgroundColor(Color.WHITE)
-                                .setTitleColor(Color.BLACK)
-                                .setFirstTextFieldHint("Enter New Name")
-                                       .setFirstTextFieldBorderColor(Color.BLACK)
-                                       .setFirstTextFieldHintColor(Color.BLACK)
-                                       .setFirstTextFieldTextColor(Color.BLACK)
-                                       .setFirstTextFieldTextColor(Color.BLACK)
-                                .setFirstButtonText("Make Changes")
-                        .setFirstButtonColor(Color.LTGRAY)
-                        .setFirstButtonTextColor(Color.BLACK)
-                                       .setSecondButtonText("Cancel")
-                                       .setSecondButtonColor(Color.CYAN)
-                                       .setSecondButtonTextColor(Color.BLACK)
-                        .withFirstButtonListner(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Map<String,Object> map = new HashMap<>();
-                                Log.i("auth",auth.getUid());
-                                map.put("name",String.valueOf(flatDialog.getFirstTextField().toString()));
-                                reference.child("name").setValue(flatDialog.getFirstTextField().toString());
-                                Toast.makeText(MyAccount.this, "Name Changed Successfully", Toast.LENGTH_SHORT).show();
-                                flatDialog.dismiss();
-                            }
-                        })
-                                       .withSecondButtonListner(new View.OnClickListener() {
-                                           @Override
-                                           public void onClick(View view) {
-                                               flatDialog.dismiss();
-                                           }
-                                       });
+        if (tag.equals("admin")) {
+            reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(Objects.requireNonNull(auth.getUid()));
+            int id = item.getId();
+            switch (id) {
+                case R.id.changeNameBottomSheet:
+                    FlatDialog flatDialog = new FlatDialog(MyAccount.this);
+                    flatDialog.setTitle("Enter New Name")
+                            .setBackgroundColor(Color.WHITE)
+                            .setTitleColor(Color.BLACK)
+                            .setFirstTextFieldHint("Enter New Name")
+                            .setFirstTextFieldBorderColor(Color.BLACK)
+                            .setFirstTextFieldHintColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstButtonText("Make Changes")
+                            .setFirstButtonColor(Color.LTGRAY)
+                            .setFirstButtonTextColor(Color.BLACK)
+                            .setSecondButtonText("Cancel")
+                            .setSecondButtonColor(Color.CYAN)
+                            .setSecondButtonTextColor(Color.BLACK)
+                            .withFirstButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Map<String, Object> map = new HashMap<>();
+                                    Log.i("auth", auth.getUid());
+                                    map.put("name", String.valueOf(flatDialog.getFirstTextField().toString()));
+                                    reference.child("name").setValue(flatDialog.getFirstTextField().toString());
+                                    Toast.makeText(MyAccount.this, "Name Changed Successfully", Toast.LENGTH_SHORT).show();
+                                    flatDialog.dismiss();
+                                }
+                            })
+                            .withSecondButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    flatDialog.dismiss();
+                                }
+                            });
 
-                flatDialog.create();
-                flatDialog.show();
-                modalBottomSheetDialog.dismiss();
-                break;
-            case R.id.changeEmailBottomSheet:
-                FlatDialog flatDialog1 = new FlatDialog(MyAccount.this);
-                flatDialog1.setTitle("Enter New Name")
-                        .setBackgroundColor(Color.WHITE)
-                        .setTitleColor(Color.BLACK)
-                        .setFirstTextFieldBorderColor(Color.BLACK)
-                        .setFirstTextFieldHintColor(Color.BLACK)
-                        .setFirstTextFieldTextColor(Color.BLACK)
-                        .setFirstTextFieldHint("Enter New Name")
-                        .setFirstButtonText("Make Changes")
-                        .setFirstButtonColor(Color.LTGRAY)
-                        .setFirstButtonTextColor(Color.BLACK)
-                        .setSecondButtonText("Cancel")
-                        .setSecondButtonColor(Color.CYAN)
-                        .setSecondButtonTextColor(Color.BLACK)
-                        .withFirstButtonListner(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Map<String,Object> map = new HashMap<>();
-                                Log.i("auth",auth.getUid());
-                                map.put("name",String.valueOf(flatDialog1.getFirstTextField().toString()));
-                                reference.child("email").setValue(flatDialog1.getFirstTextField().toString());
-                                Toast.makeText(MyAccount.this, "Email Changed Successfully", Toast.LENGTH_SHORT).show();
-                                flatDialog1.dismiss();
-                            }
-                        })
-                        .withSecondButtonListner(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                flatDialog1.dismiss();
-                            }
-                        });
+                    flatDialog.create();
+                    flatDialog.show();
+                    modalBottomSheetDialog.dismiss();
+                    break;
+                case R.id.changeEmailBottomSheet:
+                    FlatDialog flatDialog1 = new FlatDialog(MyAccount.this);
+                    flatDialog1.setTitle("Enter New Name")
+                            .setBackgroundColor(Color.WHITE)
+                            .setTitleColor(Color.BLACK)
+                            .setFirstTextFieldBorderColor(Color.BLACK)
+                            .setFirstTextFieldHintColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstTextFieldHint("Enter New Name")
+                            .setFirstButtonText("Make Changes")
+                            .setFirstButtonColor(Color.LTGRAY)
+                            .setFirstButtonTextColor(Color.BLACK)
+                            .setSecondButtonText("Cancel")
+                            .setSecondButtonColor(Color.CYAN)
+                            .setSecondButtonTextColor(Color.BLACK)
+                            .withFirstButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Map<String, Object> map = new HashMap<>();
+                                    Log.i("auth", auth.getUid());
+                                    map.put("name", String.valueOf(flatDialog1.getFirstTextField().toString()));
+                                    reference.child("email").setValue(flatDialog1.getFirstTextField().toString());
+                                    Toast.makeText(MyAccount.this, "Email Changed Successfully", Toast.LENGTH_SHORT).show();
+                                    flatDialog1.dismiss();
+                                }
+                            })
+                            .withSecondButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    flatDialog1.dismiss();
+                                }
+                            });
 
-                flatDialog1.create();
-                flatDialog1.show();
-                modalBottomSheetDialog.dismiss();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + id);
+                    flatDialog1.create();
+                    flatDialog1.show();
+                    modalBottomSheetDialog.dismiss();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + id);
+            }
+        }else if(tag.equals("restaurant")){
+            reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(Objects.requireNonNull(auth.getUid()));
+            int id = item.getId();
+            switch (id){
+                case R.id.changeNameBottomSheetRestaurant:
+//                    reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(Objects.requireNonNull(auth.getUid()));
+                    FlatDialog flatDialog = new FlatDialog(MyAccount.this);
+                    flatDialog.setTitle("Enter New Name")
+                            .setBackgroundColor(Color.WHITE)
+                            .setTitleColor(Color.BLACK)
+                            .setFirstTextFieldHint("Enter New Name")
+                            .setFirstTextFieldBorderColor(Color.BLACK)
+                            .setFirstTextFieldHintColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstButtonText("Make Changes")
+                            .setFirstButtonColor(Color.LTGRAY)
+                            .setFirstButtonTextColor(Color.BLACK)
+                            .setSecondButtonText("Cancel")
+                            .setSecondButtonColor(Color.CYAN)
+                            .setSecondButtonTextColor(Color.BLACK)
+                            .withFirstButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    if(flatDialog.getFirstTextField().length() == 0){
+                                        Toast.makeText(MyAccount.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }else {
+                                        reference.child("name").setValue(flatDialog.getFirstTextField().toString());
+                                        Toast.makeText(MyAccount.this, "Name Changed Successfully", Toast.LENGTH_SHORT).show();
+                                        flatDialog.dismiss();
+                                    }
+                                }
+                            })
+                            .withSecondButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Toast.makeText(MyAccount.this, "Cancelled", Toast.LENGTH_SHORT).show();
+                                    flatDialog.dismiss();
+                                }
+                            });
+
+                    flatDialog.create();
+                    flatDialog.show();
+                    modalBottomSheetDialog.dismiss();
+                    break;
+                case R.id.changeEmailBottomSheetRestaurant:
+                    FlatDialog flatDialog1 = new FlatDialog(MyAccount.this);
+                    flatDialog1.setTitle("Enter New Name")
+                            .setBackgroundColor(Color.WHITE)
+                            .setTitleColor(Color.BLACK)
+                            .setFirstTextFieldBorderColor(Color.BLACK)
+                            .setFirstTextFieldHintColor(Color.BLACK)
+                            .setFirstTextFieldTextColor(Color.BLACK)
+                            .setFirstTextFieldHint("Enter New Name")
+                            .setFirstButtonText("Make Changes")
+                            .setFirstButtonColor(Color.LTGRAY)
+                            .setFirstButtonTextColor(Color.BLACK)
+                            .setSecondButtonText("Cancel")
+                            .setSecondButtonColor(Color.CYAN)
+                            .setSecondButtonTextColor(Color.BLACK)
+                            .withFirstButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    if(flatDialog1.getFirstTextField().length() == 0){
+                                        Toast.makeText(MyAccount.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }else {
+                                        reference.child("email").setValue(flatDialog1.getFirstTextField().toString());
+                                        Toast.makeText(MyAccount.this, "Email Changed Successfully", Toast.LENGTH_SHORT).show();
+                                        flatDialog1.dismiss();
+                                    }
+                                }
+                            })
+                            .withSecondButtonListner(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    flatDialog1.dismiss();
+                                }
+                            });
+
+                    flatDialog1.create();
+                    flatDialog1.show();
+                    modalBottomSheetDialog.dismiss();
+                    break;
+            }
         }
     }
 }
