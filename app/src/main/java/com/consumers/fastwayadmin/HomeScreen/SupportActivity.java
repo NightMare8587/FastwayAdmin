989939b408f15f0f8a6f917cd3e073ae2489b588
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-05-24 09:35:29 +0530

    jebnwfojnonwef

--- app/src/main/java/com/consumers/fastwayadmin/HomeScreen/SupportActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/SupportActivity.java
@@ -1,37 +1,60 @@
 package com.consumers.fastwayadmin.HomeScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.consumers.fastwayadmin.R;
+import com.consumers.fastwayadmin.demoFrag;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SupportActivity extends AppCompatActivity{
     ListView listView;
+    ListView faqList;
+    FragmentManager fragmentManager;
     List<String> genarlQuery = new ArrayList<String>();
+    List<String> faQQuery = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
         genarlQuery.add("Live Chat With Us");
         genarlQuery.add("Email Us");
+        fragmentManager = getSupportFragmentManager();
+        initialise();
         listView = findViewById(R.id.genralQueryListView);
+        faqList = findViewById(R.id.faqListView);
+        demoFrag demoFrag = new demoFrag();
+        ArrayAdapter<String> faqAdapter = new ArrayAdapter<String>(this,R.layout.list,faQQuery);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list,genarlQuery);
+        faqList.setAdapter(faqAdapter);
         listView.setAdapter(arrayAdapter);
 
+
+        faqList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_top,R.anim.fade_in).replace(R.id.supportactivity, demoFrag).commit();
+//                        startActivity(new Intent(SupportActivity.this, PaymentAndRefunds.class));
+//                        overridePendingTransition(R.anim.slide_in_top,R.anim.slide_out_bottom);
+                        break;
+                    case 1:
+
+                        break;
+                }
+            }
+        });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -48,5 +71,10 @@ public class SupportActivity extends AppCompatActivity{
                 }
             }
         });
+    }
+
+    private void initialise() {
+        faQQuery.add("Payments And Refunds");
+        faQQuery.add("Cashbacks And Offers");
     }
 }
---
+++ app/src/main/java/com/consumers/fastwayadmin/demoFrag.java
@@ -0,0 +1,22 @@
+package com.consumers.fastwayadmin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class demoFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.demo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
