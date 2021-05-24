60b700ad83afbcec2f148555ad433e54f6e0f44d
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-05-24 11:20:06 +0530

    changes

---
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/PaymentQueries/PaymentFeeQuery.java
@@ -0,0 +1,16 @@
+package com.consumers.fastwayadmin.HomeScreen.PaymentQueries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.consumers.fastwayadmin.R;
+
+public class PaymentFeeQuery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_fee_query);
+    }
+}
---
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/RefundQueries/RefundCancellationQuery.java
@@ -0,0 +1,16 @@
+package com.consumers.fastwayadmin.HomeScreen.RefundQueries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.consumers.fastwayadmin.R;
+
+public class RefundCancellationQuery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refund_cancellation_query);
+    }
+}
