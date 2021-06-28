6f08d31645d84a23993706e2de2860d4bd7429ac
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-06-28 08:24:46 +0530

    Update CustomOffer.java

--- app/src/main/java/com/consumers/fastwayadmin/DiscountCombo/CustomOffer.java
+++ app/src/main/java/com/consumers/fastwayadmin/DiscountCombo/CustomOffer.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.consumers.fastwayadmin.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class CustomOffer extends AppCompatActivity {
-
+    List<String> dishName = new ArrayList<>();
+    FirebaseAuth auth;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_offer);
+        auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(Objects.requireNonNull(auth.getUid())).child("List of Dish");
+
     }
 }
