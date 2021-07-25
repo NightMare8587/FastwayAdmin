c79b972bebaa1a810cf474b822127a6e9d392cf3
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-07-25 09:45:11 +0530

    Update SupportActivity.java

--- app/src/main/java/com/consumers/fastwayadmin/HomeScreen/SupportActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/SupportActivity.java
@@ -2,8 +2,6 @@ package com.consumers.fastwayadmin.HomeScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,8 +18,8 @@ public class SupportActivity extends AppCompatActivity{
     ListView listView;
     ListView faqList;
 
-    List<String> genarlQuery = new ArrayList<String>();
-    List<String> faQQuery = new ArrayList<String>();
+    List<String> genarlQuery = new ArrayList<>();
+    List<String> faQQuery = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,40 +29,34 @@ public class SupportActivity extends AppCompatActivity{
         initialise();
         listView = findViewById(R.id.genralQueryListView);
         faqList = findViewById(R.id.faqListView);
-        ArrayAdapter<String> faqAdapter = new ArrayAdapter<String>(this,R.layout.list,faQQuery);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list,genarlQuery);
+        ArrayAdapter<String> faqAdapter = new ArrayAdapter<>(this, R.layout.list, faQQuery);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list, genarlQuery);
         faqList.setAdapter(faqAdapter);
         listView.setAdapter(arrayAdapter);
 
 
-        faqList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
-                    case 0:
-                        startActivity(new Intent(SupportActivity.this, FaqActivity.class));
-                        break;
-                    case 1:
-                        startActivity(new Intent(SupportActivity.this, ReportCustomer.class));
-                        break;
-                }
+        faqList.setOnItemClickListener((parent, view, position, id) -> {
+            switch (position){
+                case 0:
+                    startActivity(new Intent(SupportActivity.this, FaqActivity.class));
+                    break;
+                case 1:
+                    startActivity(new Intent(SupportActivity.this, ReportCustomer.class));
+                    break;
             }
         });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
-                    case 0:
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            switch (position){
+                case 0:
 //                        Toast.makeText(SupportActivity.this, "Work in Progress :)", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(SupportActivity.this, LiveChatActivity.class));
-                        break;
-                    case 1:
-                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                        emailIntent.setType("plain/text");
-                        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "fastway8587@gmail.com" });
-                        startActivity(emailIntent);
-                        break;
-                }
+                    startActivity(new Intent(SupportActivity.this, LiveChatActivity.class));
+                    break;
+                case 1:
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setType("plain/text");
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "fastway8587@gmail.com" });
+                    startActivity(emailIntent);
+                    break;
             }
         });
     }
