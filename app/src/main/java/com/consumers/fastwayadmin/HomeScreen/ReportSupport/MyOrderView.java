31826fa96726855ce803c6c13d7bfa0bb1ce217f
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-08-02 09:13:07 +0530

    major

---
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/ReportSupport/MyOrderView.java
@@ -0,0 +1,143 @@
+package com.consumers.fastwayadmin.HomeScreen.ReportSupport;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.consumers.fastwayadmin.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MyOrderView extends RecyclerView.Adapter<MyOrderView.holder>  {
+   List<String> timeOfOrder;
+   List<String> foodOrderTime;
+   List<List<String>> finalList;
+   List<List<String>> finalListPrice;
+   List<List<String>> finalType;
+   List<String> resId;
+   Context context;
+   List<String> type = new ArrayList<>();
+   public MyOrderView(List<List<String>> finalType, Context context, List<String> resId,
+                      List<String> type, List<String> timeOfOrder, List<String> foodOrderTime,
+                      List<List<String>> finalList, List<List<String>> finalListPrice) {
+
+       this.finalListPrice = finalListPrice;
+       this.finalList = finalList;
+       this.finalType = finalType;
+       this.foodOrderTime = foodOrderTime;
+       this.context = context;
+       this.resId = resId;
+       this.type = type;
+       this.timeOfOrder = timeOfOrder;
+   }
+
+   @NonNull
+   @Override
+   public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+       View view = layoutInflater.inflate(R.layout.recent_orders_card,parent,false);
+       return new holder(view);
+   }
+
+   @SuppressLint("SetTextI18n")
+   @Override
+   public void onBindViewHolder(@NonNull holder holder, int position) {
+       DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().getRoot().child("Users").child(resId.get(position));
+       databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot snapshot) {
+           holder.name.setText(snapshot.child("name").getValue(String.class));
+//               holder.price.setText(snapshot.child("email").getValue(String.class));
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+
+       @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+       long milis = Long.parseLong(timeOfOrder.get(position));
+       Calendar calendar = Calendar.getInstance();
+       calendar.setTimeInMillis(milis);
+       holder.time.setText("" + dateFormat.format(calendar.getTime()));
+//       if(!dishImage.get(position).isEmpty()) {
+//           Picasso.get().load(dishImage.get(position)).resize(100, 100).into(holder.imageView);
+//           Log.i("empty","not");
+//       }
+       List<String> priceList = new ArrayList<>(finalListPrice.get(position));
+       ArrayAdapter<String> priceAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1,priceList);
+       List<String> myList = new ArrayList<>(finalList.get(position));
+       ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, myList);
+       holder.listView.setAdapter(adapter);
+       holder.priceList.setAdapter(priceAdapter);
+       ViewGroup.LayoutParams param = holder.listView.getLayoutParams();
+       param.height = 150*myList.size();
+       holder.listView.setLayoutParams(param);
+       holder.listView.requestLayout();
+       ViewGroup.LayoutParams params = holder.priceList.getLayoutParams();
+       param.height = 150*priceList.size();
+       holder.priceList.setLayoutParams(param);
+       holder.priceList.requestLayout();
+
+
+   }
+
+   @Override
+   public int getItemCount() {
+       return finalList.size();
+   }
+
+
+   public static class holder extends RecyclerView.ViewHolder{
+       TextView name;
+       ImageView imageView;
+       Button rate;
+       ListView listView,priceList;
+       TextView time;
+       CardView cardView;
+       public holder(@NonNull View itemView) {
+           super(itemView);
+           name = itemView.findViewById(R.id.recentOrderUserName);
+           rate = itemView.findViewById(R.id.recentOrderReportButton);
+           imageView = itemView.findViewById(R.id.myOrderDishImage);
+           time = itemView.findViewById(R.id.timeOfOrderDate);
+           listView = itemView.findViewById(R.id.myListViewOrderDish);
+           priceList = itemView.findViewById(R.id.myListViewOrderPrice);
+           cardView = itemView.findViewById(R.id.myOrdersCard);
+       }
+   }
+
+}
--- app/src/main/java/com/consumers/fastwayadmin/HomeScreen/ReportSupport/ReportCustomer.java
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/ReportSupport/ReportCustomer.java
@@ -1,16 +1,188 @@
 package com.consumers.fastwayadmin.HomeScreen.ReportSupport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.consumers.fastwayadmin.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ReportCustomer extends AppCompatActivity {
-
+    List<List<String>> finalListPrice = new ArrayList<>();
+    List<String> dishName = new ArrayList<>();
+    List<List<String>> finalList = new ArrayList<>();
+    List<List<String>> finalResId = new ArrayList<>();
+    List<List<String>> finalType = new ArrayList<>();
+    List<String> dishImage = new ArrayList<>();
+    List<String> dishPrice = new ArrayList<>();
+    List<String> type = new ArrayList<>();
+    List<String> resId = new ArrayList<>();
+    List<String> timeOfOrder = new ArrayList<>();
+    List<String> foodOrderTime = new ArrayList<>();
+    int count = 1;
+    DatabaseReference reference;
+    RecyclerView recyclerView;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_customer);
+        initialise();
+        reference.child("Recent Orders").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(!snapshot.exists()){
+                    Toast.makeText(ReportCustomer.this, "No Order Made :)", Toast.LENGTH_SHORT).show();
+                }else{
+//                    Toast.makeText(ReportCustomer.this, "Click on your orders to download their invoice, If needed.", Toast.LENGTH_SHORT).show();
+                    timeOfOrder.clear();
+                    foodOrderTime.clear();
+                    finalResId.clear();
+                    type.clear();
+                    finalList.clear();
+                    finalType.clear();
+                    finalListPrice.clear();
+                    dishPrice.clear();
+                    dishName.clear();
+                    dishImage.clear();
+                    resId.clear();
+                    count = 1;
+                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                        timeOfOrder.add(String.valueOf(dataSnapshot.getKey()));
+                        for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                            resId.add(String.valueOf(dataSnapshot1.getKey()));
+                            for(DataSnapshot dataSnapshot2 : dataSnapshot1.getChildren()){
+                                dishImage.add(dataSnapshot2.child("image").getValue(String.class));
+                                dishName.add(dataSnapshot2.child("name").getValue(String.class));
+                                dishPrice.add(dataSnapshot2.child("price").getValue(String.class));
+                                type.add(dataSnapshot2.child("type").getValue(String.class));
+                                foodOrderTime.add(dataSnapshot2.child("time").getValue(String.class));
+
+                            }
+
+                            finalList.add(new ArrayList<>(dishName));
+                            finalListPrice.add(new ArrayList<>(dishPrice));
+                            finalType.add(new ArrayList<>(type));
+                            type.clear();
+                            dishName.clear();
+                            dishPrice.clear();
+                        }
+
+                    }
+//                    Toast.makeText(MyOrders.this, ""+restaurantAddress.toString(), Toast.LENGTH_SHORT).show();
+                    recyclerView.setAdapter(new MyOrderView(finalType,ReportCustomer.this,resId,
+                            type,timeOfOrder,foodOrderTime,finalList,finalListPrice));
+                    Log.i("info",finalList.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        reference.child("Recent Orders").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                updateChild();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                updateChild();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                updateChild();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                updateChild();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void updateChild(){
+        reference.child("Recent Orders").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(!snapshot.exists()){
+                    Toast.makeText(ReportCustomer.this, "No Order Made :)", Toast.LENGTH_SHORT).show();
+                }else{
+                    timeOfOrder.clear();
+                    foodOrderTime.clear();
+                    type.clear();
+                    finalList.clear();
+                    finalListPrice.clear();
+                    dishPrice.clear();
+                    dishName.clear();
+                    dishImage.clear();
+                    resId.clear();
+                    finalType.clear();
+                    count = 1;
+                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                        timeOfOrder.add(String.valueOf(dataSnapshot.getKey()));
+                        for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                            resId.add(String.valueOf(dataSnapshot1.getKey()));
+                            for(DataSnapshot dataSnapshot2 : dataSnapshot1.getChildren()){
+                                dishImage.add(dataSnapshot2.child("image").getValue(String.class));
+                                dishName.add(dataSnapshot2.child("name").getValue(String.class));
+                                dishPrice.add(dataSnapshot2.child("price").getValue(String.class));
+                                type.add(dataSnapshot2.child("type").getValue(String.class));
+                                foodOrderTime.add(dataSnapshot2.child("time").getValue(String.class));
+
+                            }
+                            finalType.add(new ArrayList<>(type));
+                            type.clear();
+                            finalList.add(new ArrayList<>(dishName));
+                            finalListPrice.add(new ArrayList<>(dishPrice));
+                            dishName.clear();
+                            dishPrice.clear();
+                        }
+
+                    }
+//                    Toast.makeText(MyOrders.this, ""+restaurantAddress.toString(), Toast.LENGTH_SHORT).show();
+                    recyclerView.setAdapter(new MyOrderView(finalType,ReportCustomer.this,resId,
+                            type,timeOfOrder,foodOrderTime,finalList,finalListPrice));
+                    Log.i("info",finalList.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initialise() {
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Restaurants").child(Objects.requireNonNull(auth.getUid()));
+        recyclerView = findViewById(R.id.myOrderRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 }
