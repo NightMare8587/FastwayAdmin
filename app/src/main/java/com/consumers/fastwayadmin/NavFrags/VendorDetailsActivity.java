5c07d4be4ab67b31f6a4baeeeaa42ce19b12efec
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-08-13 09:55:11 +0530

    major

--- app/src/main/java/com/consumers/fastwayadmin/NavFrags/VendorDetailsActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/VendorDetailsActivity.java
@@ -1,13 +1,20 @@
 package com.consumers.fastwayadmin.NavFrags;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.consumers.fastwayadmin.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class VendorDetailsActivity extends AppCompatActivity {
     String name,email;
+    TabLayout tabLayout;
+    ViewPager viewPager2;
+    EditText nameEdit,emailEdit,phoneEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +25,33 @@ public class VendorDetailsActivity extends AppCompatActivity {
     private void initialise() {
         name = getIntent().getStringExtra("name");
         email = getIntent().getStringExtra("email");
+        nameEdit = findViewById(R.id.nameVendorEditText);
+        emailEdit = findViewById(R.id.emailVendorEditText);
+        phoneEdit = findViewById(R.id.numberVendorEditText);
+
+        nameEdit.setText(name);
+        emailEdit.setText(email);
+        tabLayout = findViewById(R.id.vendorTabLayout);
+        viewPager2 = findViewById(R.id.vendorViewPager);
+        tabLayout.addTab(tabLayout.newTab().setText("Bank"));
+        tabLayout.addTab(tabLayout.newTab().setText("UPI"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),
+                tabLayout.getTabCount());
+
+        viewPager2.setAdapter(adapter);
+        viewPager2.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/VendorTabs/UPIvendor.java
@@ -0,0 +1,30 @@
+package com.consumers.fastwayadmin.NavFrags.VendorTabs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.consumers.fastwayadmin.R;
+
+public class UPIvendor extends Fragment {
+
+    public UPIvendor(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.upi_vendor_layout,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
