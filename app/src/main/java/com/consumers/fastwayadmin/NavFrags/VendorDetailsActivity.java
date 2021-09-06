00d34647efb528f910e87bcda68d4816286b0475
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-09-06 11:27:29 +0530

    Update VendorDetailsActivity.java

--- app/src/main/java/com/consumers/fastwayadmin/NavFrags/VendorDetailsActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/VendorDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.consumers.fastwayadmin.NavFrags;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
@@ -52,17 +53,22 @@ import karpuzoglu.enes.com.fastdialog.PositiveClick;
 import karpuzoglu.enes.com.fastdialog.Type;
 
 public class VendorDetailsActivity extends AppCompatActivity {
-    String name,email,phone,acNumber,acName,acIFSC;
+    String name, email, phone, acNumber, acName, acIFSC;
     String url = "https://intercellular-stabi.000webhostapp.com/CheckoutPayouts/addBenificiary.php";
     FirebaseAuth mAuth;
     String mauthId;
+    FastDialog fastDialog;
+    String genratedToken = "";
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    DatabaseReference reference;
     SharedPreferences sharedPreferences;
     String neftUrl = "https://intercellular-stabi.000webhostapp.com/CheckoutPayouts/payoutNeft.php";
     SharedPreferences.Editor editor;
     String authToken = "https://intercellular-stabi.000webhostapp.com/CheckoutPayouts/authBEarerToken.php";
     String authURL = "https://payout-api.cashfree.com/payout/v1/authorize";
     Button proceed;
-    EditText nameEdit,emailEdit,phoneEdit,accountNumber,accountName,IFSCcode;
+    EditText nameEdit, emailEdit, phoneEdit, accountNumber, accountName, IFSCcode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,88 +76,56 @@ public class VendorDetailsActivity extends AppCompatActivity {
         initialise();
         mAuth = FirebaseAuth.getInstance();
         mauthId = String.valueOf(mAuth.getUid());
-        sharedPreferences = getSharedPreferences("VendorID",MODE_PRIVATE);
+        fastDialog = new FastDialogBuilder(VendorDetailsActivity.this,Type.PROGRESS)
+                .progressText("Adding Please Wait")
+                .setAnimation(Animations.SLIDE_TOP)
+                .create();
+        reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(Objects.requireNonNull(auth.getUid()));
+        sharedPreferences = getSharedPreferences("VendorID", MODE_PRIVATE);
         editor = sharedPreferences.edit();
         proceed.setOnClickListener(v -> {
-            if(nameEdit.getText().toString().equals("")){
+            if (nameEdit.getText().toString().equals("")) {
                 nameEdit.setError("Field can't be empty");
                 nameEdit.requestFocus();
                 return;
-            }else if(emailEdit.getText().toString().equals("")){
+            } else if (emailEdit.getText().toString().equals("")) {
                 emailEdit.setError("Field can't be empty");
                 emailEdit.requestFocus();
                 return;
-            }else if(phoneEdit.getText().toString().equals("")){
+            } else if (phoneEdit.getText().toString().equals("")) {
                 phoneEdit.setError("Field can't be empty");
                 phoneEdit.requestFocus();
                 return;
-            }else if(accountNumber.getText().toString().equals("")){
+            } else if (accountNumber.getText().toString().equals("")) {
                 accountNumber.requestFocus();
                 accountNumber.setError("Field can't be empty");
                 return;
-            }else if(accountName.getText().toString().equals("")){
+            } else if (accountName.getText().toString().equals("")) {
                 accountName.requestFocus();
                 accountName.setError("Field can't be empty");
                 return;
-            }else if(IFSCcode.getText().toString().equals("")){
+            } else if (IFSCcode.getText().toString().equals("")) {
                 IFSCcode.requestFocus();
                 IFSCcode.setError("Field can't be empty");
                 return;
-            }else{
+            } else {
                 name = nameEdit.getText().toString();
                 email = emailEdit.getText().toString();
                 phone = phoneEdit.getText().toString();
                 acName = accountName.getText().toString();
                 acNumber = accountNumber.getText().toString();
                 acIFSC = IFSCcode.getText().toString();
-                FirebaseAuth auth = FirebaseAuth.getInstance();
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(Objects.requireNonNull(auth.getUid()));
 
-                KAlertDialog kAlertDialog = new KAlertDialog(VendorDetailsActivity.this,KAlertDialog.WARNING_TYPE)
+
+                KAlertDialog kAlertDialog = new KAlertDialog(VendorDetailsActivity.this, KAlertDialog.WARNING_TYPE)
                         .setTitleText("Warning")
                         .setContentText("Do you sure wanna continue with above credentials?")
                         .setConfirmText("Yes")
                         .setCancelText("No, Wait")
                         .setConfirmClickListener(kAlertDialog1 -> {
-                            RequestQueue requestQueue = Volley.newRequestQueue(VendorDetailsActivity.this);
-                            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                                @Override
-                                public void onResponse(String response) {
-                                    if(response != null){
-                                        Log.i("response",response);
-                                        Toast.makeText(VendorDetailsActivity.this, "Changes Saved Successfully", Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.i("error",error.getLocalizedMessage() + " ");
-                                }
-                            }){
-                                @NonNull
-                                @Override
-                                public Map<String, String> getParams() throws AuthFailureError {
-                                    Map<String,String> params = new HashMap<>();
-                                    params.put("name",name + "");
-                                    params.put("email",email + "");
-                                    params.put("phone",phone);
-                                    params.put("AccountNumber",acNumber);
-                                    params.put("AccountName",acName);
-                                    params.put("ifscCode",acIFSC);
-                                    params.put("vendorID",mauthId);
-                                    Log.i("details",name +  " " + mauthId + " ");
-                                    return params;
-                                }
-                            };
-                            requestQueue.add(stringRequest);
-                            VendorBankClass vendorBankClass = new VendorBankClass(name,email,acNumber,acName,acIFSC,phone,mauthId);
-                            editor.putString("vendorDetails","yes");
-                            editor.putString("accountNumber",acNumber);
-                            editor.putString("accountName",acName);
-                            editor.putString("ifscCode",acIFSC);
-                            editor.apply();
-                            reference.child("Bank Details").setValue(vendorBankClass);
+
+                            new MakePayout().execute();
+                            fastDialog.show();
                             kAlertDialog1.dismissWithAnimation();
                         }).setCancelClickListener(new KAlertDialog.KAlertClickListener() {
                             @Override
@@ -166,25 +140,6 @@ public class VendorDetailsActivity extends AppCompatActivity {
 //                verifyAuthURL();
             }
         });
-    }
-
-    private void verifyAuthURL() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, authURL, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                return super.getHeaders();
-            }
-        };
     }
 
 
@@ -202,5 +157,108 @@ public class VendorDetailsActivity extends AppCompatActivity {
         emailEdit.setText(email);
 
     }
+    public class MakePayout extends AsyncTask<Void,Void,Void>{
 
+        @Override
+        protected Void doInBackground(Void... voids) {
+            RequestQueue requestQueue = Volley.newRequestQueue(VendorDetailsActivity.this);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, neftUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("response",response);
+                    genratedToken = response.trim();
+                    new AuthorizeToken().execute();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            requestQueue.add(stringRequest);
+            return null;
+        }
+    }
+
+    public class AuthorizeToken extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            RequestQueue requestQueue = Volley.newRequestQueue(VendorDetailsActivity.this);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, authToken, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("response",response);
+                    if(response.trim().equals("Token is valid")){
+                        new AddBenificiary().execute();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("token",genratedToken);
+                    return params;
+                }
+            };
+            requestQueue.add(stringRequest);
+            return null;
+        }
+    }
+
+    public class AddBenificiary extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            RequestQueue requestQueue = Volley.newRequestQueue(VendorDetailsActivity.this);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("response",response.trim());
+                    fastDialog.dismiss();
+                    if(response.trim().equals("SUCCESS"))
+                        Toast.makeText(VendorDetailsActivity.this, "Beneficiary added successfully", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(VendorDetailsActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+                    VendorBankClass vendorBankClass = new VendorBankClass(name, email, acNumber, acName, acIFSC, phone, mauthId);
+                    editor.putString("vendorDetails", "yes");
+                    editor.putString("accountNumber", acNumber);
+                    editor.putString("address", acName);
+                    editor.putString("ifscCode", acIFSC);
+                    editor.apply();
+                    reference.child("Bank Details").setValue(vendorBankClass);
+                    params.put("token",genratedToken);
+                    params.put("benID",mauthId);
+                    params.put("name",name);
+                    params.put("email",email);
+                    params.put("phone",phone);
+                    params.put("bankAc",acNumber);
+                    params.put("ifsc",acIFSC);
+                    params.put("address",acName);
+                    return params;
+                }
+            };
+            requestQueue.add(stringRequest);
+            return null;
+        }
+    }
 }
