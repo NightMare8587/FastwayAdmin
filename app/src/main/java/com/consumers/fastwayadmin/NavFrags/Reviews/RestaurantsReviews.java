1ce646cd2395174ede6259177d0299a94719f4b2
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-02-04 10:50:23 +0530

    Update RestaurantsReviews.java

--- app/src/main/java/com/consumers/fastwayadmin/NavFrags/Reviews/RestaurantsReviews.java
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/Reviews/RestaurantsReviews.java
@@ -1,16 +1,15 @@
 package com.consumers.fastwayadmin.NavFrags.Reviews;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.consumers.fastwayadmin.R;
 import com.google.firebase.auth.FirebaseAuth;
