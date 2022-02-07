3af52b0ec9f427ae34a71aadc4a11b1971915699
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-02-07 10:22:12 +0530

    Update LiveChatActivity.java

--- app/src/main/java/com/consumers/fastwayadmin/HomeScreen/LiveChat/LiveChatActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/HomeScreen/LiveChat/LiveChatActivity.java
@@ -15,16 +15,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import com.consumers.fastwayadmin.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -34,7 +30,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
-
 
 import org.json.JSONObject;
 
@@ -50,10 +45,13 @@ public class LiveChatActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<String> message = new ArrayList<>();
     FirebaseAuth auth;
-    DatabaseReference reference;
-    String FURL  = "https://fcm.googleapis.com/fcm/send";
+    DatabaseReference reference; boolean connectedWithFastway = false;
     SharedPreferences sharedPreferences;
+    DatabaseReference liveTalkWithAdmin;
     SharedPreferences.Editor editor;
+    String FURL  = "https://fcm.googleapis.com/fcm/send";
+    SharedPreferences saveInfo;
+    SharedPreferences.Editor saveEdit;
     LinearLayoutManager linearLayoutManager;
     List<String> time = new ArrayList<>();
     List<String> leftOrRight = new ArrayList<>();
@@ -64,11 +62,33 @@ public class LiveChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_chat);
         initialise();
+        saveInfo = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        saveEdit = saveInfo.edit();
         linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        liveTalkWithAdmin = FirebaseDatabase.getInstance().getReference().getRoot().child("Complaints").child("LiveChat");
+        liveTalkWithAdmin.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    connectedWithFastway = true;
+                    saveEdit.putString("liveChat","yes");
+                    saveEdit.apply();
+                }else {
+                    saveEdit.remove("liveChat");
+                    saveEdit.apply();
+                    botReply();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         linearLayoutManager.setStackFromEnd(true);
         reference = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(Objects.requireNonNull(auth.getUid()));
-        botReply();
-        reference.child("Live Chat").addListenerForSingleValueEvent(new ValueEventListener() {
+
+        reference.child("Live Chat").limitToLast(15).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
@@ -99,10 +119,16 @@ public class LiveChatActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(editText.getText().toString().equals("")){
                     Toast.makeText(LiveChatActivity.this, "Enter Some Text :)", Toast.LENGTH_SHORT).show();
-                    return;
                 }else{
                     String messa = editText.getText().toString();
+                    if(connectedWithFastway){
+                        String time = String.valueOf(System.currentTimeMillis());
 
+                        liveChatClass liveChatClass = new liveChatClass(messa,time,"0");
+                        reference.child("Live Chat").child(time + "").setValue(liveChatClass);
+                        editText.setText("");
+                        return;
+                    }
                     if(messa.equals("2")){
                         modiFiedBotReply();
                         String time = String.valueOf(System.currentTimeMillis());
@@ -121,10 +147,41 @@ public class LiveChatActivity extends AppCompatActivity {
 
                         new Handler().postDelayed(() -> {
                             String stime = String.valueOf(System.currentTimeMillis());
+                            liveTalkWithAdmin.child("id").setValue(auth.getUid());
+                            liveTalkWithAdmin.child("type").setValue("Admin");
                             liveChatClass liveChatClass1 = new liveChatClass("Connecting you with an agent. Avg wait time 2 min",stime,"1");
                             reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass1);
                         },500);
                         editText.setText("");
+                        RequestQueue requestQueue = Volley.newRequestQueue(LiveChatActivity.this);
+                        JSONObject main = new JSONObject();
+                        try {
+                            main.put("to", "/topics/" + "FastwayLiveChat");
+                            JSONObject notification = new JSONObject();
+                            notification.put("title", "Live Talk Request");
+                            notification.put("body", "A user wants to talk right now... " + editText.getText().toString());
+                            main.put("notification", notification);
+
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, FURL, main, response -> {
+
+                            }, error -> {
+                                Log.i("info",error.getLocalizedMessage() + " s");
+                                Toast.makeText(getApplicationContext(), error.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
+                            }) {
+                                @Override
+                                public Map<String, String> getHeaders() {
+                                    Map<String, String> header = new HashMap<>();
+                                    header.put("content-type", "application/json");
+                                    header.put("authorization", "key=AAAAsigSEMs:APA91bEUF9ZFwIu84Jctci56DQd0TQOepztGOIKIBhoqf7N3ueQrkClw0xBTlWZEWyvwprXZmZgW2MNywF1pNBFpq1jFBr0CmlrJ0wygbZIBOnoZ0jP1zZC6nPxqF2MAP6iF3wuBHD2R");
+                                    return header;
+                                }
+                            };
+
+                            requestQueue.add(jsonObjectRequest);
+                        } catch (Exception e) {
+                            Log.i("info",e.getLocalizedMessage() + " s");
+                            Toast.makeText(getApplicationContext(), e.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
+                        }
                         return;
                     }
 
@@ -139,6 +196,36 @@ public class LiveChatActivity extends AppCompatActivity {
                             liveChatClass liveChatClass1 = new liveChatClass("Please Enter the number for callback\nNo need to add +91",stime,"1");
                             reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass1);
                         },500);
+
+                        RequestQueue requestQueue = Volley.newRequestQueue(LiveChatActivity.this);
+                        JSONObject main = new JSONObject();
+                        try {
+                            main.put("to", "/topics/" + "FastwayLiveChat");
+                            JSONObject notification = new JSONObject();
+                            notification.put("title", "CallBack Request");
+                            notification.put("body", "Callback request from number " + editText.getText().toString());
+                            main.put("notification", notification);
+
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, FURL, main, response -> {
+
+                            }, error -> {
+                                Log.i("info",error.getLocalizedMessage() + " s");
+                                Toast.makeText(getApplicationContext(), error.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
+                            }) {
+                                @Override
+                                public Map<String, String> getHeaders() {
+                                    Map<String, String> header = new HashMap<>();
+                                    header.put("content-type", "application/json");
+                                    header.put("authorization", "key=AAAAsigSEMs:APA91bEUF9ZFwIu84Jctci56DQd0TQOepztGOIKIBhoqf7N3ueQrkClw0xBTlWZEWyvwprXZmZgW2MNywF1pNBFpq1jFBr0CmlrJ0wygbZIBOnoZ0jP1zZC6nPxqF2MAP6iF3wuBHD2R");
+                                    return header;
+                                }
+                            };
+
+                            requestQueue.add(jsonObjectRequest);
+                        } catch (Exception e) {
+                            Log.i("info",e.getLocalizedMessage() + " s");
+                            Toast.makeText(getApplicationContext(), e.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
+                        }
                         editText.setText("");
                         return;
                     }
@@ -164,17 +251,11 @@ public class LiveChatActivity extends AppCompatActivity {
                             notification.put("body", "Callback request from number " + editText.getText().toString());
                             main.put("notification", notification);
 
-                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, FURL, main, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, FURL, main, response -> {
 
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.i("info",error.getLocalizedMessage() + " s");
-                                    Toast.makeText(getApplicationContext(), error.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
-                                }
+                            }, error -> {
+                                Log.i("info",error.getLocalizedMessage() + " s");
+                                Toast.makeText(getApplicationContext(), error.getLocalizedMessage() + "null", Toast.LENGTH_SHORT).show();
                             }) {
                                 @Override
                                 public Map<String, String> getHeaders() {
@@ -200,30 +281,22 @@ public class LiveChatActivity extends AppCompatActivity {
                     reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass);
                     editText.setText("");
 
-                    StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try{
+                    StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, URL, response -> {
+                        try{
 //                                Toast.makeText(LiveChatActivity.this, ""+response.toString(), Toast.LENGTH_SHORT).show();
 
-                                liveChatClass liveChatClass = new liveChatClass(response.toString() + "","" + System.currentTimeMillis(),"1");
+                            liveChatClass liveChatClass12 = new liveChatClass(response + "","" + System.currentTimeMillis(),"1");
 
-                                reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass);
-                                if(response.equals("Wrong Input"))
-                                    botReply();
-                            }catch (Exception e){
-                                Toast.makeText(LiveChatActivity.this, "" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                            }
+                            reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass12);
+                            if(response.equals("Wrong Input"))
+                                botReply();
+                        }catch (Exception e){
+                            Toast.makeText(LiveChatActivity.this, "" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    }, new Response.ErrorListener() {
+                    }, error -> Toast.makeText(LiveChatActivity.this, "" + error.getLocalizedMessage(), Toast.LENGTH_SHORT).show()){
+                        @NonNull
                         @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(LiveChatActivity.this, "" + error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }){
-                        @Nullable
-                        @Override
-                        protected Map<String, String> getParams() throws AuthFailureError {
+                        protected Map<String, String> getParams() {
                             Map<String,String> params = new HashMap<>();
                             params.put("response",messa);
                             return params;
@@ -272,17 +345,14 @@ public class LiveChatActivity extends AppCompatActivity {
         liveChatClass liveChatClass = new liveChatClass("Hi I am Fastway Bot",System.currentTimeMillis() + "","1");
         reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                liveChatClass liveChatClass = new liveChatClass("Choose One Option\n1.Refund Status\n2.Other Options\n\n Enter number as input",System.currentTimeMillis() + "","1");
-                reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass);
-            }
+        new Handler().postDelayed(() -> {
+            liveChatClass liveChatClass1 = new liveChatClass("Choose One Option\n1.Refund Status\n2.Other Options\n\n Enter number as input",System.currentTimeMillis() + "","1");
+            reference.child("Live Chat").child(System.currentTimeMillis() + "").setValue(liveChatClass1);
         },500);
     }
 
     private void updateChild() {
-        reference.child("Live Chat").addListenerForSingleValueEvent(new ValueEventListener() {
+        reference.child("Live Chat").limitToLast(15).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
@@ -294,6 +364,17 @@ public class LiveChatActivity extends AppCompatActivity {
                         message.add(String.valueOf(dataSnapshot.child("message").getValue()));
                         time.add(String.valueOf(dataSnapshot.child("time").getValue()));
                         leftOrRight.add(String.valueOf(dataSnapshot.child("leftOr").getValue()));
+                    }
+
+                    if(message.get(message.size() - 1).equals("Connected with Fastway Agent")) {
+                        connectedWithFastway = true;
+                        saveEdit.putString("liveChat","yes");
+                        saveEdit.apply();
+                    }else if(message.get(message.size()-1).equals("Fastway Agent has left the chat")) {
+                        connectedWithFastway = false;
+                        saveEdit.remove("liveChat");
+                        saveEdit.apply();
+                        botReply();
                     }
 
                     recyclerView.setLayoutManager(linearLayoutManager);
