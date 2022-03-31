db2c6d85074fb306909c68b282076740929dc82a
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-03-31 10:08:26 +0530

    changes made

--- app/src/main/java/com/consumers/fastwayadmin/NavFrags/ResEarningTracker/ResEarningTrackerActivity.java
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/ResEarningTracker/ResEarningTrackerActivity.java
@@ -5,6 +5,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.consumers.fastwayadmin.R;
@@ -37,7 +39,9 @@ public class ResEarningTrackerActivity extends AppCompatActivity  {
     Calendar calendar;
     RecyclerView recyclerView;
     TackerAdapter tackerAdapter;
+    int totalAmountPerMonth = 0;
     Gson gson;
+    TextView totalOrdersMade,totalTransactionsMade;
     String json;
     String[] monthName = {"January", "February",
             "March", "April", "May", "June", "July",
@@ -45,6 +49,7 @@ public class ResEarningTrackerActivity extends AppCompatActivity  {
             "December"};
     List<String> allMonthsNames;
     int currentDay;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,11 +57,29 @@ public class ResEarningTrackerActivity extends AppCompatActivity  {
         resTrackInfo = getSharedPreferences("RestaurantTrackingDaily",MODE_PRIVATE);
         calendar = Calendar.getInstance();
         storeOrdersForAdminInfo = getSharedPreferences("StoreOrders",MODE_PRIVATE);
+        totalOrdersMade = findViewById(R.id.totalOrdersMadeTextViewResTransactions);
+        totalTransactionsMade = findViewById(R.id.totalTransactionAmountTextViewResTrans);
         String month = monthName[calendar.get(Calendar.MONTH)];
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         java.lang.reflect.Type type = new TypeToken<List<List<String>>>() {
         }.getType();
         gson = new Gson();
+        if(storeOrdersForAdminInfo.contains(month)) {
+            json = storeOrdersForAdminInfo.getString(month, "");
+            List<List<String>> mainDataListText = gson.fromJson(json, type);
+            List<String> dateText = new ArrayList<>(mainDataListText.get(0));
+            List<String> transIDText = new ArrayList<>(mainDataListText.get(1));
+            List<String> userIDText = new ArrayList<>(mainDataListText.get(2));
+            List<String> orderAmountListText = new ArrayList<>(mainDataListText.get(3));
+            for (int i = 0; i < orderAmountListText.size(); i++) {
+                totalAmountPerMonth += Integer.parseInt(orderAmountListText.get(i));
+            }
+            totalOrdersMade.setText("Total Transactions Made: " + dateText.size());
+            totalTransactionsMade.setText("Total Transactions Made: \u20B9" + totalAmountPerMonth);
+        }else{
+            totalOrdersMade.setText("Total Transactions Made: " + 0);
+            totalTransactionsMade.setText("Total Transactions Made: \u20B9" + 0);
+        }
         recyclerView = findViewById(R.id.monthNamesListViewRes);
         allMonthsNames = new ArrayList<>(Arrays.asList(monthName));
         currentDay = calendar.get(Calendar.DAY_OF_MONTH);
@@ -67,18 +90,7 @@ public class ResEarningTrackerActivity extends AppCompatActivity  {
         recyclerView.setAdapter(new TackerAdapter(allMonthsNames));
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
                 new IntentFilter("custom-message"));
-//        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
-//        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-//        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
-//        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
-//        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
-//        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-//        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+
         for(int i=0;i<12;i++){
             if(storeOrdersForAdminInfo.contains(monthName[i])) {
                 json = storeOrdersForAdminInfo.getString(month,"");
@@ -96,15 +108,34 @@ public class ResEarningTrackerActivity extends AppCompatActivity  {
                 mBarChart.addBar(new BarModel(0.f, 0xFF123456));
             }
         }
-
         mBarChart.startAnimation();
     }
     public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
         @Override
         public void onReceive(Context context, Intent intent) {
             // Get extra data included in the Intent
             String MonthName = intent.getStringExtra("month");
-            Toast.makeText(ResEarningTrackerActivity.this,MonthName + "" ,Toast.LENGTH_SHORT).show();
+            if(storeOrdersForAdminInfo.contains(MonthName)){
+                java.lang.reflect.Type type = new TypeToken<List<List<String>>>() {
+                }.getType();
+                json = storeOrdersForAdminInfo.getString(MonthName, "");
+                List<List<String>> mainDataListText = gson.fromJson(json, type);
+                List<String> dateText = new ArrayList<>(mainDataListText.get(0));
+                List<String> transIDText = new ArrayList<>(mainDataListText.get(1));
+                List<String> userIDText = new ArrayList<>(mainDataListText.get(2));
+                List<String> orderAmountListText = new ArrayList<>(mainDataListText.get(3));
+                totalAmountPerMonth = 0;
+                for (int i = 0; i < orderAmountListText.size(); i++) {
+                    totalAmountPerMonth += Integer.parseInt(orderAmountListText.get(i));
+                }
+                totalOrdersMade.setText("Total Transactions Made: " + dateText.size());
+                totalTransactionsMade.setText("Total Transactions Made: \u20B9" + totalAmountPerMonth);
+            }else{
+                totalOrdersMade.setText("Total Transactions Made: " + 0);
+                totalTransactionsMade.setText("Total Transactions Made: \u20B9" + 0);
+                Toast.makeText(context, "No transactions made in Month " + MonthName, Toast.LENGTH_SHORT).show();
+            }
         }
     };
 
