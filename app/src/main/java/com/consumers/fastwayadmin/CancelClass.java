1b3c266733494f74cd2d8043695da4baeed57b47
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-04-05 20:45:30 +0530

    Update CancelClass.java

--- app/src/main/java/com/consumers/fastwayadmin/CancelClass.java
+++ app/src/main/java/com/consumers/fastwayadmin/CancelClass.java
@@ -2,7 +2,6 @@ package com.consumers.fastwayadmin;
 
 public class CancelClass {
     public String referId,referAmount,orderID;
-
     public CancelClass(String referId, String referAmount, String orderID) {
         this.referId = referId;
         this.referAmount = referAmount;
