57c3a9ea84754f5c150f7553113efb280f4c7472
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-07-31 10:27:10 +0530

    changes

--- app/src/main/java/com/consumers/fastwayadmin/NavFrags/Reviews/RestaurantsReviews.java
+++ app/src/main/java/com/consumers/fastwayadmin/NavFrags/Reviews/RestaurantsReviews.java
@@ -1,5 +1,6 @@
 package com.consumers.fastwayadmin.NavFrags.Reviews;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.consumers.fastwayadmin.NavFrags.FastwayPremiumActivites.FastwayPremiums;
 import com.consumers.fastwayadmin.R;
+import com.developer.kalert.KAlertDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -35,6 +38,7 @@ public class RestaurantsReviews extends AppCompatActivity {
     List<String> rating = new ArrayList<>();
     Button resStaffReview;
     List<String> customerName = new ArrayList<>();
+    SharedPreferences sharedPreferences;
     List<String> customerReview = new ArrayList<>();
 
     @Override
@@ -42,32 +46,49 @@ public class RestaurantsReviews extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_reviews);
         initialise();
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    customerID.clear();
-                    rating.clear();
-                    customerName.clear();
-                    customerReview.clear();
+        sharedPreferences = getSharedPreferences("AdminPremiumDetails",MODE_PRIVATE);
+        if(sharedPreferences.contains("status") && sharedPreferences.getString("status","").equals("active")) {
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        customerID.clear();
+                        rating.clear();
+                        customerName.clear();
+                        customerReview.clear();
 
-                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                        customerID.add(String.valueOf(dataSnapshot.getKey()));
-                        customerReview.add(String.valueOf(dataSnapshot.child("reviews").getValue()));
-                        customerName.add(String.valueOf(dataSnapshot.child("name").getValue()));
-                        rating.add(String.valueOf(dataSnapshot.child("rating").getValue()));
-                    }
-                    progressBar.setVisibility(View.INVISIBLE);
-                    recyclerView.setAdapter(new ReviewAdapter(customerID,rating,customerName,customerReview,RestaurantsReviews.this));
-                }else
-                    progressBar.setVisibility(View.INVISIBLE);
-            }
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            customerID.add(String.valueOf(dataSnapshot.getKey()));
+                            customerReview.add(String.valueOf(dataSnapshot.child("reviews").getValue()));
+                            customerName.add(String.valueOf(dataSnapshot.child("name").getValue()));
+                            rating.add(String.valueOf(dataSnapshot.child("rating").getValue()));
+                        }
+                        progressBar.setVisibility(View.INVISIBLE);
+                        recyclerView.setAdapter(new ReviewAdapter(customerID, rating, customerName, customerReview, RestaurantsReviews.this));
+                    } else
+                        progressBar.setVisibility(View.INVISIBLE);
+                }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-        });
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            });
+        }else{
+            progressBar.setVisibility(View.INVISIBLE);
+            KAlertDialog kAlertDialog = new KAlertDialog(RestaurantsReviews.this,KAlertDialog.WARNING_TYPE)
+                    .setTitleText("Premium").setContentText("Subscribe premium to see reviews about your restaurant")
+                    .setConfirmText("Subscribe").setCancelText("Exit").setConfirmClickListener(click -> {
+                        click.dismissWithAnimation();
+                        startActivity(new Intent(RestaurantsReviews.this, FastwayPremiums.class));
+                    }).setCancelClickListener(click -> {
+                        click.dismissWithAnimation();
+                        finish();
+                    });
+
+            kAlertDialog.setCancelable(false);
+            kAlertDialog.show();
+        }
 
 //        resStaffReview.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -100,6 +121,7 @@ public class RestaurantsReviews extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         progressBar = findViewById(R.id.restaurantReviewProgressBar);
         resStaffReview = findViewById(R.id.restaurantStaffReciewButton);
+
         staffReff = FirebaseDatabase.getInstance().getReference().getRoot().child("Admin").child(auth.getUid()).child("Restaurant Staff Reports");
     }
 }
