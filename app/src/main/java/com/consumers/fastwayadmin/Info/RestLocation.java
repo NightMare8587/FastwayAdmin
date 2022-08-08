88fb7eb9a52f37414fe0b2ef92bfe5daf5b93820
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-08-08 12:00:28 +0530

    Update RestLocation.java

--- app/src/main/java/com/consumers/fastwayadmin/Info/RestLocation.java
+++ app/src/main/java/com/consumers/fastwayadmin/Info/RestLocation.java
@@ -3,7 +3,7 @@ package com.consumers.fastwayadmin.Info;
 public class RestLocation {
     public String lat,lon;
 
-    public RestLocation(String lat,String lon){
+    public RestLocation(String lon,String lat){
         this.lat = lat;
         this.lon = lon;
     }
