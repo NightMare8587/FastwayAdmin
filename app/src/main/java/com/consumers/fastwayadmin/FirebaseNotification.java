ce72d61b16d6e21d968d86deaaeff4de84db1cbd
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-12-01 09:52:06 +0530

    Update FirebaseNotification.java

--- app/src/main/java/com/consumers/fastwayadmin/FirebaseNotification.java
+++ app/src/main/java/com/consumers/fastwayadmin/FirebaseNotification.java
@@ -1,11 +1,9 @@
 package com.consumers.fastwayadmin;
 
-import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
